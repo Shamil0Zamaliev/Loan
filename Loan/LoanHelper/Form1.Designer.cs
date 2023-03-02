913a6ad2a7617217377a72561963210542da3ebc
Shamil0Zamaliev <114811430+Shamil0Zamaliev@users.noreply.github.com>
2023-03-02 11:27:53 +0300

    Add files via upload

---
+++ Loan/LoanHelper/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace LoanHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.time = new System.Windows.Forms.TextBox();
+            this.comboprocess = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.TextBox();
+            this.procent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.difference = new System.Windows.Forms.TextBox();
+            this.monthfee = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calculateBtn.Location = new System.Drawing.Point(425, 242);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(325, 60);
+            this.calculateBtn.TabIndex = 0;
+            this.calculateBtn.Text = "Рассчитать";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // time
+            // 
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.time.Location = new System.Drawing.Point(550, 50);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(89, 26);
+            this.time.TabIndex = 2;
+            // 
+            // comboprocess
+            // 
+            this.comboprocess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboprocess.FormattingEnabled = true;
+            this.comboprocess.Items.AddRange(new object[] {
+            "Месяца/ев",
+            "Года/лет"});
+            this.comboprocess.Location = new System.Drawing.Point(645, 48);
+            this.comboprocess.Name = "comboprocess";
+            this.comboprocess.Size = new System.Drawing.Size(105, 28);
+            this.comboprocess.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(543, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Срок кредита";
+            // 
+            // sum
+            // 
+            this.sum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sum.Location = new System.Drawing.Point(50, 50);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(200, 26);
+            this.sum.TabIndex = 2;
+            // 
+            // procent
+            // 
+            this.procent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.procent.Location = new System.Drawing.Point(300, 50);
+            this.procent.Name = "procent";
+            this.procent.Size = new System.Drawing.Size(200, 26);
+            this.procent.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(297, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Процентная ставка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(47, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Сумма";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(50, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(325, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Досрочное погашение ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(78, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 4;
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(50, 168);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(200, 26);
+            this.result.TabIndex = 2;
+            // 
+            // difference
+            // 
+            this.difference.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.difference.Location = new System.Drawing.Point(547, 168);
+            this.difference.Name = "difference";
+            this.difference.Size = new System.Drawing.Size(203, 26);
+            this.difference.TabIndex = 2;
+            // 
+            // monthfee
+            // 
+            this.monthfee.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.monthfee.Location = new System.Drawing.Point(300, 168);
+            this.monthfee.Name = "monthfee";
+            this.monthfee.Size = new System.Drawing.Size(200, 26);
+            this.monthfee.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(47, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 19);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Всего выплат";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(297, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 19);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Ежемесячный платеж";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(546, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 19);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Переплата";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(804, 311);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.procent);
+            this.Controls.Add(this.difference);
+            this.Controls.Add(this.monthfee);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.comboprocess);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.calculateBtn);
+            this.Name = "Form1";
+            this.Text = "LoanHelper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.TextBox time;
+        private System.Windows.Forms.ComboBox comboprocess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sum;
+        private System.Windows.Forms.TextBox procent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.TextBox difference;
+        private System.Windows.Forms.TextBox monthfee;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Loan/LoanHelper/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoanHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            double mainsum = Convert.ToDouble(sum.Text);
+            double perc = Convert.ToDouble(procent.Text);
+            perc = perc/(100*12);
+
+
+
+            if (comboprocess.SelectedIndex == 0)
+            {
+                int months = (Convert.ToInt32(time.Text));
+                double X = Math.Round(mainsum * (perc / (1 - (Math.Pow((1 + perc), -months)))));
+                monthfee.Text = Convert.ToString(X);
+                result.Text = Convert.ToString(X * months);
+                difference.Text = Convert.ToString((X * months) - mainsum);
+            }
+            else if (comboprocess.SelectedIndex == 1)
+            {
+                int months = (12 * Convert.ToInt32(time.Text));
+                double X = Math.Round(mainsum * (perc /( 1 - (Math.Pow((1 + perc), -months)))));
+                monthfee.Text = Convert.ToString(X);
+                result.Text = Convert.ToString(X*months);
+                difference.Text = Convert.ToString((X * months) - mainsum);
+
+            }
+        }
+    }
+}
